b625e3aaf99febd8cf8e8fe2702096ec17c12bf2
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-07-18 09:09:11 +0000

    finished with note html navbar

--- note_taker/static/js/function.ts
+++ note_taker/static/js/function.ts
@@ -1,8 +1,8 @@
 // start with the work for note url
 
-let delete_note_btn = document.querySelector<HTMLTableElement>(".delete-note-btn")
-let home_note_btn = document.querySelector<HTMLTableElement>(".home-note-btn")
-let edit_note_btn = document.querySelector<HTMLTableElement>(".edit-note-btn")
+let delete_note_btn = document.querySelector<HTMLDivElement>(".delete-note-btn")
+let home_note_btn = document.querySelector<HTMLDivElement>(".home-note-btn")
+let edit_note_btn = document.querySelector<HTMLDivElement>(".edit-note-btn")
 let home_note_div = document.querySelector<HTMLDivElement>(".home-note-content")
 let edit_note_div = document.querySelector<HTMLDivElement>(".edit-note-content")
 
