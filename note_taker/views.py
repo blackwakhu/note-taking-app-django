886d9d10a7ec0faaa693e89a7be6f496e52bb22b
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-07-18 06:46:39 +0000

    done with updating

--- note_taker/views.py
+++ note_taker/views.py
@@ -22,6 +22,12 @@ def new_notes(request):
 
 def note(request, note_id):
   item = get_object_or_404(Notes, pk=note_id)
+  if request.method == "POST":
+    item.title = request.POST.get('title')
+    item.content = request.POST.get('content')
+    item.category = request.POST.get('category')
+    item.save()
+    return redirect("note", note_id)
   return render(request, 'note.html', {"item": item})
 
 def delete_note(request, note_id):
